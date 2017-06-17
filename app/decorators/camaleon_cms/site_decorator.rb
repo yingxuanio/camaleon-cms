b25433b0c4a5d94f47cefa5cdacc311b4dfb0fa7
Ivan Lee <cestivan@163.com>
2017-06-17 21:56:21 +0800

    update yingxuan things

--- app/decorators/camaleon_cms/site_decorator.rb
+++ app/decorators/camaleon_cms/site_decorator.rb
@@ -8,11 +8,11 @@ class CamaleonCms::SiteDecorator < CamaleonCms::TermTaxonomyDecorator
   # return logo url for this site
   # default: this url will be returned if logo is not present.
   def the_logo(default = nil)
-    object.get_option("logo") || (default || "#{h.asset_url("camaleon_cms/camaleon.png")}")
+    object.get_option("logo") || (default || "#{h.asset_url("camaleon_cms/logo.png")}")
   end
 
   def the_icon
-    object.get_option("icon") || h.asset_url('camaleon_cms/favicon.ico')
+    object.get_option("icon") || h.asset_url('camaleon_cms/favicon.png')
   end
 
   # return all contents from this site registered for post_type = slug (filter visibility, hidden, expired, ...)
